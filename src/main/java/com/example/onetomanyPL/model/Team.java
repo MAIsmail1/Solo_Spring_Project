abd6de4d48570623c8208009d5215ea73e2a4003
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-05-19 09:39:43 +0100

    added non-arg constructor

--- src/main/java/com/example/onetomanyPL/model/Team.java
+++ src/main/java/com/example/onetomanyPL/model/Team.java
@@ -1,4 +1,18 @@
 package com.example.onetomanyPL.model;
 
 public class Team {
+    private String name;
+    private int founded;
+    private String ground;
+    private String position;
+
+    public Team() {
+    }
+
+    public Team(String name, int founded, String ground, String position) {
+        this.name = name;
+        this.founded = founded;
+        this.ground = ground;
+        this.position = position;
+    }
 }
