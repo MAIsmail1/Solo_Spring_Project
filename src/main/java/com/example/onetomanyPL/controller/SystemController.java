01b02c987557912cbaf5a78dd7f266cdc9c9272b
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-05-19 11:20:23 +0100

    restcontroller on each controller

--- src/main/java/com/example/onetomanyPL/controller/SystemController.java
+++ src/main/java/com/example/onetomanyPL/controller/SystemController.java
@@ -1,7 +1,11 @@
 package com.example.onetomanyPL.controller;
 
 import com.example.onetomanyPL.repository.SystemRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SystemController {
     private final SystemRepository systemRepository;
     public SystemController(SystemRepository systemRepository) {
--- src/main/java/com/example/onetomanyPL/controller/TeamController.java
+++ src/main/java/com/example/onetomanyPL/controller/TeamController.java
@@ -1,7 +1,9 @@
 package com.example.onetomanyPL.controller;
 
 import com.example.onetomanyPL.repository.TeamRepository;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TeamController {
     private final TeamRepository teamRepository;
     public TeamController(TeamRepository teamRepository) {
