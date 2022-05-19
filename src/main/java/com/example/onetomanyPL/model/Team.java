fb43c24fb6887261e8bbcc82361fd25ca914b4c5
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-05-19 09:40:47 +0100

    added getters/setters for team

--- src/main/java/com/example/onetomanyPL/model/Team.java
+++ src/main/java/com/example/onetomanyPL/model/Team.java
@@ -15,4 +15,36 @@ public class Team {
         this.ground = ground;
         this.position = position;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFounded() {
+        return founded;
+    }
+
+    public void setFounded(int founded) {
+        this.founded = founded;
+    }
+
+    public String getGround() {
+        return ground;
+    }
+
+    public void setGround(String ground) {
+        this.ground = ground;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
